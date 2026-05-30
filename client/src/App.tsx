f1a5dc71f5bd5b12340848bfb61e619d9b16d551
Naveen Mastamardi <mastamardinaveen524@gmail.com>
2026-05-30 08:36:26 +0530

    Initial setup, Added:Register & Login in backend, Added Auth system

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-zinc-950 text-white">
+      <h1 className="text-4xl font-bold">
+        Enterprise Messenger 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
